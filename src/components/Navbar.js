e73f1ebdc8e8b11b5314f558aca896b8f21c32ec
Sandeep26899 <91952149+Sandeep26899@users.noreply.github.com>
2021-10-20 18:15:10 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,12 +14,12 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about">About</Link>
-              </li>
+              </li> */}
             </ul>
             
             <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
