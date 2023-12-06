1f66330a36534bce1f24db8b4df1f1c92c269900
Sandeep  Jaiswal <sandeep.jaiswal@antiersolutions.com>
2023-12-06 16:46:46 +0530

    add speech to text feature

--- src/App.js
+++ src/App.js
@@ -1,64 +1,54 @@
-import './App.css';
-import Navbar from './components/Navbar.js';
-import TextForm from './components/TextForm.js';
-import Alert from './components/Alert.js';
-import React, {useState} from 'react';
-import About from './components/About';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import TextForm from "./components/TextForm.js";
+import Alert from "./components/Alert.js";
+import React, { useState } from "react";
 
 
 function App() {
-  
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-  setAlert({
-    msg: message,
-    type: type
-  })  
-  setTimeout(()=> { 
-    setAlert(null);
-  }, 1500);  
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
   const toggleMode = () => {
-    if(mode === 'dark'){
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert('Light Mode Enabled', 'success');
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode Enabled", "success");
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042745";
+      showAlert("Dark Mode Enabled", "success");
     }
-    else{
-      setMode('dark');
-      document.body.style.backgroundColor = '#042745';
-      showAlert('Dark Mode Enabled', 'success');
-    }
-  }
+  };
 
-    return (
-      <Router>
-        <Navbar title="TextUtils" name="Sandeep" mode={mode} toggleMode={toggleMode}/> 
-        <Alert alert={alert}/>
-        
-        <Switch>
+  return (
+    <>
+      <Navbar
+        title="TextUtils"
+        name="Sandeep"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
 
-            <Route exact path="/">
-            <TextForm title="Enter Text Below To Convert:" mode={mode} showAlert={showAlert}/>
-            </Route>
-
-            <Route exact path="/about">
-              <About/>
-            </Route>
-
-        </Switch>
-      </Router>
-    );
+      <TextForm
+        title="Enter Text Below To Convert:"
+        mode={mode}
+        showAlert={showAlert}
+      />
+    </>
+  );
 }
 
 export default App;
